d86f4ea3195da96c81583a992e71287c6d4bc16f
Dragon2000v <shmatko064@gmail.com>
2024-10-02 23:40:22 +0300

    add put endpoint for users

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import connect from "@/lib/db";
 import User from "@/lib/modals/users";
+import { Types } from "mongoose";
 import { NextResponse } from "next/server";
+
+const ObjectId = require("mongoose").Types.ObjectId;
 
 export const GET = async () => {
   try {
@@ -27,6 +30,51 @@ export const POST = async (request: Request) => {
     );
   } catch (error: any) {
     return new NextResponse("Error in creating user" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+export const PUT = async (request: Request) => {
+  try {
+    const body = await request.json();
+    const { userId, newUserName } = body;
+    await connect();
+    if (!userId || !newUserName) {
+      return new NextResponse(
+        JSON.stringify({ message: "ID or new username not found" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(JSON.stringify({ message: "ID is not valid" }), {
+        status: 400,
+      });
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: new ObjectId(userId) },
+      { username: newUserName },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        {
+          status: 404,
+        }
+      );
+    }
+    return new NextResponse(
+      JSON.stringify({ message: "User updated", user: updatedUser }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in updating user" + error.message, {
       status: 500,
     });
   }
